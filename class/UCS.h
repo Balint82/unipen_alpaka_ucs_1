e4132271543ebcb0f4df424ac4e2e3fb995fabd1
Major Bálint <major.balint82@gmail.com>
2023-11-06 18:37:34 +0100

    UCS operation pre

--- class/UCS.h
+++ class/UCS.h
@@ -1,6 +1,6 @@
 #include "Graph.h"
 #include "Matrix.h"
-
+#include <string.h>
 #include <iostream>
 
 #pragma once
@@ -19,118 +19,139 @@ public:
     }
 
     void makeWay(){
-        Node startNode;
-        Node destNode;
-        int row = 0;
-        int col;
-        size_t colContainer;
-        list<Node> momentNodeList;
+        Node inputStartNode, inputDestNode, startNode, destNode, min, mint;
+        int row = 0, col;
+        list<Node> allNodeList, momentNodeList;
+        vector<Node> allVector;
+        vector<double> allWeight;
         double** mapMatrix = wayMatrix.getAdjMatrix();
         int mapMatrixSize = wayGraph.addNodeForNodeCounterAndCalc();
         double itemWeight;
-        Node minWeightNode;
-        Node nodeAtIndex;
-
         set<Node> nodeSet = wayGraph.getNodeCounter();
-        vector<Node> nodesVector(nodeSet.begin(), nodeSet.end());
-        
-        
+        //vector<Node> nodesVector(nodeSet.begin(), nodeSet.end());
         cout<<"Kerem adja meg a kiindulo varost: ";
-        cin>>startNode.name;
-        startNode.weight = 0.0;
-
+        cin>>inputStartNode.name;
+        inputStartNode.weight = 0.0;
         cout<<"Kerem adja meg a celvarost: ";
-        cin>>destNode.name;
+        cin>>inputDestNode.name;
         cout<<endl;
-
         while(cin.fail()){
             cout<<"Beolvasas hiba."<<endl;
             cin.clear();
         }
 
-        if(startNode.name != destNode.name){
-            momentNodeList.push_back(startNode);
-        }
-       
+
+
+        int counter = 0;
+        while(true){
+            //sor keresés megadott kiinduló város alapján
+            auto itRow = nodeSet.find(inputStartNode);
+            if(itRow != nodeSet.end()){
+                row = distance(nodeSet.begin(), itRow);
+            } 
+            //row oké
+            cout<<"inputStartNode1: "<<inputStartNode.name<<inputStartNode.weight<<endl;
+            //oszlop választások és tárolások
+            for(col = 0; col < mapMatrixSize; col++){
+                itemWeight = mapMatrix[row][col]; //a szülő csúcs súlya hozzáadódik a gyerek csúcsok súlyához
+                if(itemWeight > 0.0 ){
+                    itemWeight = mapMatrix[row][col] + inputStartNode.weight;
+                    cout<<mapMatrix[row][col]<<" "<<inputStartNode.weight<<" "<<itemWeight<<"\t";
+                    auto itColShowDestNode = nodeSet.begin();
+                    advance(itColShowDestNode, col);
+                    destNode = *itColShowDestNode;
+                    destNode.weight = itemWeight;
+                    momentNodeList.push_back(destNode);
+                }
+
+            }
         
-        //mátrix sorai közül kiválasztani a kezdővárost
-        auto itStart = nodeSet.find(startNode);
+            cout<<endl;
+            cout<<"MNL gyerekcsucsok utan: "<<endl;
+             for(const Node& item : momentNodeList){
+               cout<<item.name<<item.weight<<"\t";
+            }
+            cout<<endl;
+            //momentNodeList oké
+            min = momentNodeList.front();
+            for(const Node& item : momentNodeList){
+                if(item.weight < min.weight){
+                    min = item;
+                }
+            }
+
+            cout<<"MomentNodelist minimum utan: "<<endl;           
+            for(const Node& item : momentNodeList){
+               cout<<item.name<<item.weight<<"\t";
+            }
+            cout<<endl;
+
+            if(inputDestNode.name == min.name){
+                cout<<"Elerted a celvarost: "<<min.name<<min.weight<<endl;
+                
+                cout<<endl;
+                for(const Node& item : momentNodeList){
+                    cout<<item.name<<item.weight<<"\t";
+                }
+                cout<<endl;
+
+                break;
+            } else{
+                inputStartNode = min;
+                momentNodeList.remove(min);
+                cout<<"inputStartNode2: "<<inputStartNode.name<<inputStartNode.weight<<endl;
+
+                cout<<"MNL else agban: "<<endl;
+                for(const Node& item : momentNodeList){
+                    cout<<item.name<<item.weight<<"\t";
+                }
+                cout<<endl;
+                cout<<endl;
+                cout<<counter<<endl;
+                   
+            }
+            counter++;
+        }
+
+    }
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//mátrix sorai közül kiválasztani a kezdővárost és a halmaz alapján megnevezni.
+        auto itStart = nodeSet.find(inputStartNode);
         if(itStart != nodeSet.end()){
             row = distance(nodeSet.begin(), itStart);
         }
+        startNode = inputStartNode;
+        startNode.weight = 0.0;
 
-        cout<<row<<endl;
-        cout<<"MomentNodelist:"<<endl;
-        for(const Node& listItem : momentNodeList){
-            cout<<listItem.name<<listItem.weight;
-        }
-        cout<<endl;
-        //a.) ha megvan a kezdő város ki kell választani a minimum súlyút, ami nem nulla
-        cout<<"mapMatrixSize: "<<mapMatrixSize<<endl;
-        
-        
-        for(col = 0; col < mapMatrixSize; col++){
-            itemWeight = mapMatrix[row][col]; //a startNode-ik sor col-adik eleme
-            cout<<mapMatrix[row][col]<<"\t";
-            
-            auto it = nodeSet.begin(); //a coladik elem keresése a halmazban
-            advance(it, col);
-            destNode = *it; //a célNode a coladik elemhez tartozó Node
 
-            destNode.weight = itemWeight; //a coladik elem súlya a mátrix érték
-            if(destNode.weight > 0.0)
-                momentNodeList.push_back(destNode); //kerüljön be a listába ha nem nulla a súlya
-        }
-        cout<<endl;
-        cout<<endl;
-        cout<<"MomentNodelist2: {";
-        for(const Node& listItem : momentNodeList){
-            cout<<listItem.name<<listItem.weight<<", ";
-        }
-        cout<<"}"<<endl;
-        cout<<endl;
+                //oszlop választások és tárolások
+                for(col = 0; col < mapMatrixSize; col++){
+                    itemWeight = mapMatrix[row][col]; //a startNode-ik sor col-adik eleme
+                    cout<<mapMatrix[row][col]<<"\t";
 
-        //a.) momentNodeListből kiválasztjuk a legkisebb költségű csúcsot
-        
-        //keressen a minWeight-nak az első olyan elemet, ami nem a kiinduló város 
-        cout<<"StartNode : "<<startNode.name<<endl;
-        for(const Node& item : momentNodeList){
-            if(item != startNode){
-                cout<<item.name<<endl;
-                destNode = item;
-                break;
-               
-            }
-        }
-
-         //minimumkeresés
-        for(const Node& listItem : momentNodeList){
-            if(listItem != startNode){
-                if(listItem.weight < destNode.weight){
-                    destNode = listItem;
+                    if(itemWeight > 0.0){
+                        auto it = nodeSet.begin(); //a col-adik elem keresése a halmazban
+                        advance(it, col);
+                        destNode.name = it->name; //a célNode a col-adik elemhez tartozó Node
+                        destNode.weight = itemWeight;
+                        destNode.weight += startNode.weight;
+                        momentNodeList.push_back(destNode);
+                    }
                 }
-            }
-        }
-
-        cout<<"MinWeightNode: ";
-        cout<<destNode.name<<destNode.weight<<endl;
 
 
-
-
-        
-        
-
-
-
-
-
-
-
-
-
-    }
-
-
-
-};
+*/
